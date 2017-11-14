efadd77e6d0f36f282da0e1f2924f6b3445c608d
Klaas van Schelven <klaas@vanschelven.com>
2017-11-14 16:41:22 +0100

    Jekyll ffe590280e0f

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,14 +17,6 @@ $(document).ready(function() {
 
 });
 
-$(document).ready(function() {
-    var vpH = $(window).height();
-    var vH = vpH - 350;
-    $('.overlay').css("height", vH);
-    $('.featured-image').css("height", vH);
-});
-
-
 $(function(){
   $('<img>').attr('src',function(){
       var imgUrl = $('div.featured-image').css('background-image');
